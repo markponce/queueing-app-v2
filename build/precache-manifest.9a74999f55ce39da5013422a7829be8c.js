ab400bec0d6721bc60aecc28539b6c2d14b2c270
Mark Ponce <markponce07@gmail.com>
2020-09-27 19:17:06 +0800

    update frontend

---
+++ build/precache-manifest.9a74999f55ce39da5013422a7829be8c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ba248ac50dae4f567b0292da12d5e9c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7c76d8dc38c1e4f50e1",
+    "url": "/static/css/main.08dc28d2.chunk.css"
+  },
+  {
+    "revision": "a9d17aa898c19c659522",
+    "url": "/static/js/2.dccbee95.chunk.js"
+  },
+  {
+    "revision": "3be25bf51018a491ef7f73dfd2dd74c4",
+    "url": "/static/js/2.dccbee95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7c76d8dc38c1e4f50e1",
+    "url": "/static/js/main.18d71715.chunk.js"
+  },
+  {
+    "revision": "e525d3728bc63dc84390",
+    "url": "/static/js/runtime-main.bdc9ad9a.js"
+  }
+]);
